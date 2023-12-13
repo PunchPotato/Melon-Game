d7e1d0689165fa4cb0c9dde07d5ce367247bc8f2
PunchPotato <joshuaharrison04@outlook.com>
2023-12-13 15:13:12 +0000

    update

--- fruits.py
+++ fruits.py
@@ -7,11 +7,16 @@ class Fruits:
         self.color = "red"
         self.pos = vector((600, 00))
         self.vel = vector(0, 0)
-        self.acc = vector(0, 0.35)
+        self.acc = vector(0, 0.45)
         self.friction = - 0.12
         self.mouse_button_pressed = False
 
-    def update(self, event):
+        self.start_point = (650, 977)
+        self.end_point = (1322, 977)
+        self.line_width = 5
+
+
+    def update(self, event, screen):
         if event.type == pygame.MOUSEBUTTONDOWN:
             self.mouse_button_pressed = True
         
@@ -21,17 +26,26 @@ class Fruits:
             self.pos += self.vel + 0.5 * self.acc
         elif self.mouse_button_pressed == False:
             self.moving_on_x_axis()
+        
+        self.container_collision()
 
     def moving_on_x_axis(self):
         mouse_x, mouse_y = pygame.mouse.get_pos()
         self.pos.x += (mouse_x - self.pos.x) * 0.1
         self.pos.y = 100
         
-        if self.pos.x <= 730: 
-            self.pos.x = 730
-        elif self.pos.x >= 1252:
-            self.pos.x = 1252
+        if self.pos.x <= 710: 
+            self.pos.x = 710
+        elif self.pos.x >= 1272:
+            self.pos.x = 1272
+
+    def container_collision(self):
+        if self.pos.y >= 960:
+            self.pos.y = 960
+            # Bounce effect
+            self.vel.y = -self.vel.y / 2
 
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (int(self.pos[0]), int(self.pos[1])), self.radius)
+        pygame.draw.line(screen, "black", self.start_point, self.end_point, self.line_width)
