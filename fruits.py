c3e1fa554ab8b1061b14fdf985d0a5fc22b52019
PunchPotato <joshuaharrison04@outlook.com>
2023-12-19 17:11:57 +0000

    update

--- fruits.py
+++ fruits.py
@@ -38,17 +38,14 @@ class Fruits:
         self.pos.x += (mouse_x - self.pos.x) * interpolation_factor
         self.pos.y = 100
 
-        if self.pos.x <= 710:
-            self.pos.x = 710
-        elif self.pos.x >= 1272:
-            self.pos.x = 1272
+        self.pos.x = max(710, min(self.pos.x, 1272))
 
     def container_collision(self):
         fruit_rect = pygame.Rect(self.pos.x - self.radius, self.pos.y - self.radius, 2 * self.radius, 2 * self.radius)
     
         fruit_container_rect = pygame.Rect(
             self.fruit_container.fruit_container_rect.x,
-            self.fruit_container.fruit_container_rect.y + 30,
+            self.fruit_container.fruit_container_rect.y,
             self.fruit_container.width,
             self.fruit_container.height
         )
@@ -58,13 +55,13 @@ class Fruits:
                 self.vel.y = -self.vel.y / 2
                 self.pos.y = min(fruit_rect.bottom, fruit_container_rect.bottom)
 
-        if fruit_rect.colliderect(fruit_container_rect) and fruit_rect.left >= fruit_container_rect.left:
+        if fruit_rect.colliderect(fruit_container_rect) and fruit_rect.right >= fruit_container_rect.left:
             if self.vel.x > 0:
-                self.vel.x = -self.vel.y / 2
+                self.vel.x = -self.vel.x / 2
         
-        if fruit_rect.colliderect(fruit_container_rect) and fruit_rect.right >= fruit_container_rect.right:
-            if self.vel.x > 0:
-                self.vel.x = -self.vel.y / 2
+        if fruit_rect.colliderect(fruit_container_rect) and fruit_rect.left <= fruit_container_rect.right:
+            if self.vel.x < 0:
+                self.vel.x = -self.vel.x / 2
                 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (int(self.pos[0]), int(self.pos[1])), self.radius)
